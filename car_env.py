e80e23ca1e6a8f92a05cf52a149fba43ec92f023
darkthecross <darkthecross@gmail.com>
2019-07-23 01:06:05 -0700

    Controller completed

--- car_env.py
+++ car_env.py
@@ -1,4 +1,4 @@
-from controller import calcWheelVels
+from controller import Controller
 import pybullet as p
 import time
 
@@ -10,12 +10,16 @@ rbt = p.loadURDF("data/balance_car.urdf", [0, 0, 2])
 
 maxForce = 50
 
-while (p.isConnected()):
+c = Controller()
+
+f = 0
+
+p.startStateLogging(p.STATE_LOGGING_VIDEO_MP4, "log.mp4")
+
+while (p.isConnected() and f < 2400):
   pos, quat = p.getBasePositionAndOrientation(rbt)
   lv, av = p.getBaseVelocity(rbt)
-
-  v1, v2 = calcWheelVels(pos, quat, lv, av)
-
+  v1, v2 = c.calcWheelVelsAngle(pos, quat, lv, av)
   p.setJointMotorControl2(bodyUniqueId=rbt, 
                           jointIndex=0, 
                           controlMode=p.VELOCITY_CONTROL,
@@ -26,7 +30,8 @@ while (p.isConnected()):
                           controlMode=p.VELOCITY_CONTROL,
                           targetVelocity = v2,
                           force = maxForce)
-
   p.stepSimulation()
-
   time.sleep(1. / 240.)
+  f = f + 1
+
+p.stopStateLogging()
--- controller.py
+++ controller.py
@@ -1,3 +1,42 @@
+import pybullet as p
+import numpy as np
+import math
 
-def calcWheelVels(pos, quat, lv, av):
-    return 1, 1
+class Controller:
+    def __init__(self):
+        self.lastP = 0
+        self.ts = 1./240.
+        self.kp = 1
+        self.kd = 0
+        self.ki = 1
+        self.inti = 0
+        self.mul = 50
+    
+    def calcAngle(self, v1, v2):
+        return math.acos(np.dot(v1[:3], v2[:3]))
+
+    # PI control for angle. May move.
+    def calcWheelVelsAngle(self, pos, quat, lv, av):
+        # Calculate the Z axis of the car body.
+        rotMat = p.getMatrixFromQuaternion(quat)
+        hMat = np.eye(4, dtype=np.float32)
+        hMat[:3, :3] = np.array(rotMat).reshape((3, 3))
+        zVec = np.array([0, 0, 1, 1])
+        rotatedZ = np.matmul(hMat, zVec)
+        # Calculate the angle of Z axis.
+        theta = self.calcAngle(zVec, rotatedZ)
+        yVec = np.array([0, 1, 0, 1])
+        rotatedY = np.matmul(hMat, yVec)
+        cross = np.cross(zVec[:3], rotatedZ[:3])
+        yTheta = self.calcAngle(cross, rotatedY)
+        if yTheta > math.pi / 2:
+            theta = -theta
+        print(theta)
+        # PID
+        dTheta = (theta - self.lastP) / self.ts
+        self.lastP = theta
+        self.inti = self.inti + theta * self.ts
+        v = self.kp * theta + self.kd * dTheta + self.ki * self.inti
+        res = self.mul * v
+        print(res)
+        return res, res
